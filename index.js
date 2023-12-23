85b3a8abf7aa13b9bf42416e852c3b5cd2104868
Артём Ситников <sitnikov.artem91@gmail.com>
2023-12-23 16:00:18 +0200

    add minor chages

--- index.js
+++ index.js
@@ -17,60 +17,60 @@ import { uaWorkLogic } from "./botUaWork.js";
 dotenv.config()
 const app = express();
 app.use(express.static('public'))
-export const bot = new Telegraf(process.env.BOT_TOKEN);
+// export const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // console.log(process.env.BOT_TOKEN);
 // BOT_TOKEN='123456789:AAGjzuxw7m3qOxXzxtoPfp1W2u61BMnDkMw' npm start
 /////початок роботи бота
-bot.start((ctx) => {
+// bot.start((ctx) => {
 
-  ctx.reply(
-    `
-Вітаю!👋🏻
-Мене звати Артем і ви знаходитесь у моєму боті. `,
-    removeKeyboard()
-  );
+//   ctx.reply(
+//     `
+// Вітаю!👋🏻
+// Мене звати Артем і ви знаходитесь у моєму боті. `,
+//     removeKeyboard()
+//   );
 
-  ctx.reply(
-    `Цей бот інформаційний.
-Якщо ви хочете написати мені, пишіть в особисте✍️`,
-    linkToUser()
-  );
-  setTimeout(() => {
-    ctx.reply("На якій мові ви хочете спілкуватися ? ", languageKeyBoard());
-  }, 3000);
-});
+//   ctx.reply(
+//     `Цей бот інформаційний.
+// Якщо ви хочете написати мені, пишіть в особисте✍️`,
+//     linkToUser()
+//   );
+//   setTimeout(() => {
+//     ctx.reply("На якій мові ви хочете спілкуватися ? ", languageKeyBoard());
+//   }, 3000);
+// });
 
-bot.action("ua", (ctx) => {
-  ctx.reply(
-    "Дякую за відповідь🇺🇦. Тепер підскажіть будь ласка з якою метою ви тут:",
-    purposeKeyBoardUA()
-  );
-});
+// bot.action("ua", (ctx) => {
+//   ctx.reply(
+//     "Дякую за відповідь🇺🇦. Тепер підскажіть будь ласка з якою метою ви тут:",
+//     purposeKeyBoardUA()
+//   );
+// });
 
-bot.action("en", (ctx) => {
-  ctx.reply(
-    "Thanks for the answer🇺🇸. Now please tell me for what purpose you are here:",
-    purposeKeyBoardEn()
-  );
-});
+// bot.action("en", (ctx) => {
+//   ctx.reply(
+//     "Thanks for the answer🇺🇸. Now please tell me for what purpose you are here:",
+//     purposeKeyBoardEn()
+//   );
+// });
 
-uaHRLogic();
-enHRLogic();
-uaWorkLogic()
+// uaHRLogic();
+// enHRLogic();
+// uaWorkLogic()
 
-bot.on("text", (ctx) => {
-  ctx.replyWithHTML(
-    `
-    <b>${ctx.chat.first_name}</b>,не пишіть самостійно текст😥.
-Якщо ви хочете почати, натисніть кнопку почати нижче
-    `,
-    getMainMenu()
-  );
-});
+// bot.on("text", (ctx) => {
+//   ctx.replyWithHTML(
+//     `
+//     <b>${ctx.chat.first_name}</b>,не пишіть самостійно текст😥.
+// Якщо ви хочете почати, натисніть кнопку почати нижче
+//     `,
+//     getMainMenu()
+//   );
+// });
 
 
 
-bot.launch();
+// bot.launch();
 
 app.listen(() => console.log(`My server is running on port 4000`));
